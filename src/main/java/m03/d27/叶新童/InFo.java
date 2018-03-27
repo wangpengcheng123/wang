3491ddc9bdb473ce6478f87f02d64b2d3b403ec3
langwangzui <2915419165@qq.com>
2018-03-27 13:40:35 +0800

    ye

---
+++ src/main/java/m03/d27/叶新童/InFo.java
@@ -0,0 +1,62 @@
+/**
+ * Project Name:dt59.jdbc
+ * File Name:InFo.java
+ * Package Name:dt59.cangku
+ * Date:2018-3-26обнГ4:27:56
+ * Copyright (c) 2018, bluemobi All Rights Reserved.
+ */
+
+package m03.d27;
+/**
+ * Description:   <br/>
+ * Date:     2018-3-26 обнГ4:27:56 <br/>
+ * @author   LangWang
+ * @version
+ * @see
+ */
+public class InFo {
+//    private int Id;
+//    private String Name;
+//    private int Age;
+//    public int getId() {
+//        return Id;
+//    }
+//    public void setId(int id) {
+//        Id = id;
+//    }
+//    public String getName() {
+//        return Name;
+//    }
+//    public void setName(String name) {
+//        Name = name;
+//    }
+//    public int getAge() {
+//        return Age;
+//    }
+//    public void setAge(int age) {
+//        Age = age;
+//    }
+    private int cid;
+    private String pname;
+    private String cname;
+    public int getCid() {
+        return cid;
+    }
+    public void setCid(int cid) {
+        this.cid = cid;
+    }
+    public String getPname() {
+        return pname;
+    }
+    public void setPname(String pname) {
+        this.pname = pname;
+    }
+    public String getCname() {
+        return cname;
+    }
+    public void setCname(String cname) {
+        this.cname = cname;
+    }
+    
+}
+
---
+++ src/main/java/m03/d27/叶新童/TextDemo01.java
@@ -0,0 +1,66 @@
+/**
+ * Project Name:dt59.homework
+ * File Name:TextDemo01.java
+ * Package Name:m03.d27
+ * Date:2018-3-27ÏÂÎç12:38:21
+ * Copyright (c) 2018, bluemobi All Rights Reserved.
+ */
+
+package m03.d27;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ * Description:   <br/>
+ * Date:     2018-3-27 ÏÂÎç12:38:21 <br/>
+ * @author   LangWang
+ * @version
+ * @see
+ */
+public class TextDemo01 {
+    Connection ct=null;
+    PreparedStatement pst=null;
+    ResultSet res=null;
+    private String url="jdbc:mysql://localhost:3306/mydata";
+    public Connection getCon(){
+        try {
+         Class.forName("com.mysql.jdbc.Driver");   
+         ct=DriverManager.getConnection(url,"root","root");
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+        return ct;
+    }
+    public void Demo(){
+        try {
+            int cid = 0;
+            String cname=null;
+            String pname=null;
+            String sql="SELECT b.`cid`,b.`cname`,a.`pname` FROM province a INNER JOIN city b ON a.`pid`=b.`pid`;";
+            pst=getCon().prepareStatement(sql);
+            res=pst.executeQuery();
+            while(res.next()){
+               cid=res.getInt("cid");
+               cname=res.getString("cname");
+               pname=res.getString("pname");
+               System.out.println(cid+"\t"+cname+"\t"+pname);
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }finally{
+            
+        }
+        
+    }
+    
+    public static void main(String[] args) {
+       TextDemo01 td=new TextDemo01();
+       td.Demo();
+    }
+}
+
