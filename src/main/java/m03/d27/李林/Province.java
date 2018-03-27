6889058f1b559bd47e7bbc5afba7868568d6f4b8
1132407257 <1132407257@qq.com>
2018-03-27 17:56:50 +0800

    lilin18.3.27

---
+++ src/main/java/m03/d27/李林/Province.java
@@ -0,0 +1,33 @@
+/**
+ * Project Name:WE_JDBC
+ * File Name:Province.java
+ * Package Name:mydata
+ * Date:2018年3月27日下午4:11:15
+ * Copyright (c) 2018, bluemobi All Rights Reserved.
+ */
+
+package mydata;
+/**
+ * Description:   <br/>
+ * Date:     2018年3月27日 下午4:11:15 <br/>
+ * @author   11324
+ * @version
+ * @see
+ */
+public class Province {
+    private int pid;
+    private String pname;
+    public int getPid() {
+        return pid;
+    }
+    public void setPid(int pid) {
+        this.pid = pid;
+    }
+    public String getPname() {
+        return pname;
+    }
+    public void setPname(String pname) {
+        this.pname = pname;
+    }
+}
+
